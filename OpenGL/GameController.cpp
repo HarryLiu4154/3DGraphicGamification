750a31e638b463cd771284c363da3ba82854fe2e
Shenghao Liu <lishengh@sheridancollege.ca>
2023-11-24 12:02:18 -0500

    week11

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -27,7 +27,7 @@ void GameController::Initialize()
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_CULL_FACE);
-	glCullFace(GL_FRONT);
+	//glCullFace(GL_FRONT);
 	srand(time(0));
 
 	// Create a default perspective camera
